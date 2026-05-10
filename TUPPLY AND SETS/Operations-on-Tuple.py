f24025768f62979f081eabe2cd886ff34e6c65a4
amrutkaraarush425-netizen <amrutkaraarush425@gmail.com>
2026-05-10 20:37:15 +0530

    tuple

---
+++ TUPPLY AND SETS/Operations-on-Tuple.py
@@ -0,0 +1,37 @@
+# Empty tuple
+my_tuple = ()
+print(my_tuple)
+
+#Tuple having integers
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+# tupe with mixed datatypes
+my_tuple = (1, "hello", 3,4)
+print(my_tuple)
+
+#nested tuple
+my_tuple = ("mouse", [8, 4, 6, 3], (1, 2, 3))
+
+# Accessing tuple elements using indexing
+my_tuple = ('p', 'e', 'r', 'm', 'i', 't')
+print(my_tuple[0])
+print(my_tuple[5])
+
+# nested tuple
+n_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+
+# nested index
+print(n_tuple[0][3])
+#output: s
+print(n_tuple[1][1])
+#output: 4
+print(n_tuple[2][2])
+#output: 3
+
+# slicing
+print("sliced:", my_tuple[1:4])
+
+# Iterating through tuple
+for letter in (my_tuple):
+    print("hello", letter)
